457fd837e6586fc34dbc67b49d1a378c3e25d7e3
abeidahmed <abeidahmed92@gmail.com>
2022-05-22 10:58:33 +0400

    test autocomplete

--- packages/auto-complete-element/spec/index.spec.ts
+++ packages/auto-complete-element/spec/index.spec.ts
@@ -1,1 +1,198 @@
-export default {};
+import { expect, fixture, html } from '@open-wc/testing';
+import '../src';
+import type AutoCompleteElement from '../src';
+
+describe('AutoCompleteElement', () => {
+  describe('default behavior and attributes', () => {
+    let el: AutoCompleteElement;
+    let input: HTMLInputElement;
+    let list: HTMLElement;
+    let options: NodeListOf<HTMLElement>;
+
+    beforeEach(async () => {
+      el = await fixture(html`
+        <auto-complete for="list">
+          <input type="text" />
+          <ul id="list">
+            <li role="option">Player</li>
+            <li role="option" data-autocomplete-value="Orange">Taxi</li>
+            <li role="option" hidden>Uno</li>
+          </ul>
+        </auto-complete>
+      `);
+
+      input = el.querySelector('input');
+      list = el.querySelector('ul');
+      options = list.querySelectorAll('[role="option"]');
+    });
+
+    it('sets the default attributes', () => {
+      expect(input).to.have.attribute('spellcheck', 'false');
+      expect(input).to.have.attribute('autocomplete', 'off');
+    });
+
+    it('opens the menu on focus', () => {
+      expect(list).to.have.attribute('hidden');
+      input.focus();
+
+      expect(list).not.to.have.attribute('hidden');
+    });
+
+    it('resets the options on focus', () => {
+      input.focus();
+
+      options.forEach((option) => {
+        expect(option).not.to.have.attribute('hidden');
+      });
+    });
+
+    it('closes the menu on input blur', () => {
+      input.focus();
+      expect(list).not.to.have.attribute('hidden');
+
+      input.blur();
+      expect(list).to.have.attribute('hidden');
+    });
+  });
+
+  describe('with no previous aria-selected option', () => {
+    let el: AutoCompleteElement;
+    let input: HTMLInputElement;
+    let list: HTMLElement;
+    let options: NodeListOf<HTMLElement>;
+
+    beforeEach(async () => {
+      el = await fixture(html`
+        <auto-complete for="list">
+          <input type="text" />
+          <ul id="list">
+            <li role="option">Player</li>
+            <li role="option">Taxi</li>
+          </ul>
+        </auto-complete>
+      `);
+
+      input = el.querySelector('input');
+      list = el.querySelector('ul');
+      options = list.querySelectorAll('[role="option"]');
+    });
+
+    it('activates the first option', () => {
+      input.focus();
+
+      expect(options[0]).to.have.attribute('data-tracking');
+    });
+  });
+
+  describe('with previous aria-selected option', () => {
+    let el: AutoCompleteElement;
+    let input: HTMLInputElement;
+    let list: HTMLElement;
+    let options: NodeListOf<HTMLElement>;
+
+    beforeEach(async () => {
+      el = await fixture(html`
+        <auto-complete for="list">
+          <input type="text" />
+          <ul id="list">
+            <li role="option">Player</li>
+            <li role="option" aria-selected="true">Taxi</li>
+          </ul>
+        </auto-complete>
+      `);
+
+      input = el.querySelector('input');
+      list = el.querySelector('ul');
+      options = list.querySelectorAll('[role="option"]');
+    });
+
+    it('activates the selected option', () => {
+      input.focus();
+
+      expect(options[1]).to.have.attribute('data-tracking');
+    });
+  });
+
+  describe('keyboard interactions', () => {
+    let el: AutoCompleteElement;
+    let input: HTMLInputElement;
+    let list: HTMLElement;
+
+    beforeEach(async () => {
+      el = await fixture(html`
+        <auto-complete for="list">
+          <input type="text" />
+          <ul id="list">
+            <li role="option">Player</li>
+            <li role="option">Taxi</li>
+          </ul>
+        </auto-complete>
+      `);
+
+      input = el.querySelector('input');
+      list = el.querySelector('ul');
+    });
+
+    it('closes the menu on Escape key', () => {
+      input.focus();
+      expect(list).not.to.have.attribute('hidden');
+
+      input.dispatchEvent(new KeyboardEvent('keydown', { key: 'Escape', bubbles: true }));
+      expect(list).to.have.attribute('hidden');
+    });
+
+    it('closes the menu on alt+ArrowUp', () => {
+      input.focus();
+      expect(list).not.to.have.attribute('hidden');
+
+      input.dispatchEvent(new KeyboardEvent('keydown', { key: 'ArrowUp', altKey: true, bubbles: true }));
+      expect(list).to.have.attribute('hidden');
+    });
+
+    it('opens the menu on alt+ArrowDown', () => {
+      expect(list).to.have.attribute('hidden');
+      input.dispatchEvent(new KeyboardEvent('keydown', { key: 'ArrowDown', altKey: true, bubbles: true }));
+
+      expect(list).not.to.have.attribute('hidden');
+    });
+  });
+
+  describe('selecting an option', () => {
+    let el: AutoCompleteElement;
+    let input: HTMLInputElement;
+    let list: HTMLElement;
+    let options: NodeListOf<HTMLElement>;
+
+    beforeEach(async () => {
+      el = await fixture(html`
+        <auto-complete for="list">
+          <input type="text" />
+          <ul id="list">
+            <li role="option">Player</li>
+            <li role="option" data-autocomplete-value="Orange">Taxi</li>
+          </ul>
+        </auto-complete>
+      `);
+
+      input = el.querySelector('input');
+      list = el.querySelector('ul');
+      options = list.querySelectorAll('[role="option"]');
+    });
+
+    it('sets the input value to the textContent', () => {
+      input.focus();
+
+      options[0].dispatchEvent(new CustomEvent('combobox:commit', { bubbles: true }));
+      expect(input.value).to.equal(options[0].textContent);
+      expect(list).to.have.attribute('hidden');
+    });
+
+    it('sets the input value to the data-autocomplete-value', () => {
+      input.focus();
+
+      options[1].dispatchEvent(new CustomEvent('combobox:commit', { bubbles: true }));
+      expect(input.value).to.equal(options[1].getAttribute('data-autocomplete-value'));
+      expect(list).to.have.attribute('hidden');
+    });
+  });
+});
